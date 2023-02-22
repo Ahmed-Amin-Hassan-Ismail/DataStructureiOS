0b6555d7908807e187a4a89bae323612428b372c
Ahmed Amin <AhmedAminHassanIsmail@gmail.com>
2023-02-22 21:26:54 +0200

    add some modification to stack to be more syntax sugar and check if it empty nor not by using peek() method

--- Stack.playground/Contents.swift
+++ Stack.playground/Contents.swift
@@ -6,10 +6,20 @@ import UIKit
 // Basic implementation of the stack
 
 struct Stack<Element> {
-    private var storage: [Element] = []
     
-    init() { }
+    //MARK: - Variables
     
+    private var storage: [Element]
+    
+    
+    //MARK: - Init
+    
+    init(_ elements: [Element]) {
+        storage = elements
+    }
+    
+    
+    //MARK: - Mutating Methods
     
     mutating func push(_ element: Element) {
         storage.append(element)
@@ -19,7 +29,28 @@ struct Stack<Element> {
     mutating func pop() -> Element? {
         storage.popLast()
     }
+    
+    
+    //MARK: - Non-Mutating Methods
+    
+    func peek() -> Element? {
+        storage.last
+    }
+    
+    func isEmpty() -> Bool {
+        peek() == nil
+    }
 }
+
+//MARK: - ExpressibleByArrayLiteral
+
+extension Stack: ExpressibleByArrayLiteral {
+    init(arrayLiteral elements: Element...) {
+        storage = elements
+    }
+}
+
+//MARK: - CustomDebugStringConvertible
 
 extension Stack: CustomDebugStringConvertible {
     var debugDescription: String {
@@ -31,19 +62,17 @@ extension Stack: CustomDebugStringConvertible {
 }
 
 
-example(of: "Using Stack") {
-    var stack = Stack<Int>()
-    stack.push(5)
-    stack.push(10)
-    stack.push(15)
-    stack.push(20)
-    
-    print(stack)
-    
-    
-    stack.pop()
-    
-    print(stack)
+example(of: "initializing a stack from an array") {
+  let array = ["A", "B", "C", "D"]
+  var stack = Stack(array)
+  print(stack)
+  stack.pop()
 }
 
+
+example(of: "initializing a stack from an array literal") {
+  var stack: Stack = [1.0, 2.0, 3.0, 4.0]
+  print(stack)
+  stack.pop()
+}
 
