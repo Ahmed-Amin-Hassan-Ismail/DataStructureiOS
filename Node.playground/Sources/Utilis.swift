ac0958551546152c08d409e1816c5356bfb02e8b
Ahmed Amin <AhmedAminHassanIsmail@gmail.com>
2023-07-09 17:50:39 +0300

    create new playground to explain ios data structure

--- Node.playground/Sources/Utilis.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-
-
-public func example(of description: String, action: () -> Void) {
-    print("----example of \(description)----\n")
-    action()
-}
--- QueueArray.playground/Sources/Utilis.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-
-
-public func example(_ description: String, action: () -> Void) {
-    
-    print("---- Example of \(description) ----- \n")
-    action()
-}
